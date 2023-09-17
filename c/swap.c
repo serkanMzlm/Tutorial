e2faba2a8df2d309e6de2f173fc8955efa6df5d6
serkan <serkanmazlum306@gmail.com>
2023-09-17 09:59:08 +0300

    swap.c eklendi

---
+++ c/swap.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+void swap_2(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap_2(&x, &y);
+    printf("After swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
