97e0ec0d62efb6ec679d687361fae16ebca83b5a
serkan <serkanmazlum306@gmail.com>
2023-09-17 15:45:25 +0300

    karakter kodlama

---
+++ python/1.18_karakter_kodlama.py
@@ -0,0 +1,5 @@
+print (repr("\n")) #normalde gozukmeyen kacis satirlarini gosterilmesine yarar
+print (r"\n") #repr kisaltilmis hali
+print (ascii("\n")) #repr ayni
+print (chr(10)) # sayilarin karsiligi gosterir
+print (ord("\n")) #chr tam tersi  sayi karsiligini verir.
