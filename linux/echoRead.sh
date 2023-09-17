4c077dfa4a890d7ffe362e478d592d8766f9c759
serkan <serkanmazlum306@gmail.com>
2023-09-17 15:33:19 +0300

    echoRead eklendi

---
+++ linux/echoRead.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+name="arif mert"
+echo "Hello, my name $name"
+echo "what is your name?"
+read new_name new_surname
+echo hello $new_name , $new_surname
+echo
+my_host=`hostname`
+echo server: $my_host
