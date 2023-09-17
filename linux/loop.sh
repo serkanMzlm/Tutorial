7306a091fc49028cec8d1d91f10ff5bdf7670ca0
serkan <serkanmazlum306@gmail.com>
2023-09-17 15:33:40 +0300

    loop eklendi

---
+++ linux/loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# for
+for i in 1 2 3 4 5 
+do
+echo $i
+done
+sleep 2
+clear
+for name in ali mert murat
+do
+echo name = $name
+done
+
+for number in {1..10}
+do
+echo number = $number
+sleep 1
+clear
+done
+
