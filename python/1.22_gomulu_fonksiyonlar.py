0ebc3a15c9ea8306a6fd0f539b17651c9e9cc068
serkan <serkanmazlum306@gmail.com>
2023-09-17 15:46:52 +0300

    gömülü fonksiyonlşar

---
+++ python/1.22_gomulu_fonksiyonlar.py
@@ -0,0 +1,40 @@
+"""
+abs () mutlak deger alma
+raund () sayıları yuvarlama ikinci parametre hassasiyet
+all () boş kume olup olmadigini  kontrol eder
+any () icinde bir tane true deger varsa true doner
+ascii()
+repr()
+bool()
+bytes ()
+list()
+set()
+tuple()
+complex()
+float()
+int()
+callable()
+ord()
+oct()
+hex()
+dir()
+enumerate() nesnelere numaralandırma yapar
+exit()
+filter()
+hash()
+len()
+map()
+max()
+min()
+open()
+pow()
+print()
+quit()
+range()
+sum() dizinin içinde ki değerleri toplamak icin
+type()
+zip()
+vars() nesnelerin metot ve niteliklerini ogrenmek icin
+
+
+"""
