e337796dc31d39235e499d32538a5ef1aedd8532
serkan <serkanmazlum306@gmail.com>
2023-09-27 13:36:09 +0300

    ROS2 içinde paramlar eklendi

---
+++ ros2/cpp_param/src/publisher.cpp
@@ -0,0 +1,40 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
+#define FREQ 10
+
+// using namespace std::chrono_literals  // 1s gibi kavramların sistemde doğrudan kullanılmasını sağlarız
+using string_msg = example_interfaces::msg::String;
+
+class MyNode: public rclcpp::Node{
+public:
+    MyNode():Node("cpp_talker_node"){
+        this->declare_parameter("cpp_class_name",2);
+        this->declare_parameter("freq_pub", FREQ);
+        frequency = this->get_parameter("freq_pub").as_int();
+        rclcpp::Parameter number_ = this->get_parameter("cpp_class_name");
+        // number_ = this->get_parameter("cpp_class_name").as_int();
+        RCLCPP_INFO(this->get_logger(), "%ld) Talker started... ", number_.as_int());
+        publisher_ = this->create_publisher<string_msg>("cpp_test_counter", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0 /frequency)), std::bind(&MyNode::publisherCallback, this));
+    }
+    
+private:
+    void publisherCallback(){
+        RCLCPP_INFO(this->get_logger()," Hello");
+        string_msg msg;  // auto msg = string_msg;
+        msg.data = std::string(" Hello ");
+        publisher_->publish(msg);
+    }
+    // int number_;
+    int frequency;
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<string_msg>::SharedPtr publisher_;
+    
+};
+
+
+int main(int argc, char ** args){
+    rclcpp::init(argc, args);
+    rclcpp::spin(std::make_shared<MyNode>());
+    rclcpp::shutdown();
+}
