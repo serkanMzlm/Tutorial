07b8b95755a6b789e0cc1d52f7488de5911685bf
serkan <serkanmazlum306@gmail.com>
2023-09-28 10:57:53 +0300

    socket local client eklendi

---
+++ ipc/sockets_local_client.c
@@ -0,0 +1,88 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define SOCKET_NAME "/tmp/addNumberSocket"
+#define BUFFER_SIZE 128
+
+#define COLOR_BLK "\x1b[30m"
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GRN "\x1b[32m"
+#define COLOR_YLW "\x1b[33m"
+#define COLOR_BLE "\x1b[34m"
+#define COLOR_MGT "\x1b[35m"
+#define COLOR_CYN "\x1b[36m"
+#define COLOR_WHT "\x1b[37m"
+#define COLOR_RST "\x1b[0m"
+
+int
+main(int argc, char *argv[])
+{
+    struct sockaddr_un addr;
+    int i;
+    int ret;
+    int data_socket;
+    char buffer[BUFFER_SIZE];
+
+    data_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if (data_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+
+    memset(&addr, 0, sizeof(struct sockaddr_un)); // içinde bulunan veri temizlenir.
+
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_NAME, sizeof(addr.sun_path) - 1);
+
+    ret = connect (data_socket, (const struct sockaddr *) &addr,
+            sizeof(struct sockaddr_un));
+
+    if (ret == -1) {
+        fprintf(stderr, "The server is down.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    do{
+        printf("Send to server"COLOR_RED" (0 = exit)"COLOR_RST": ");
+        scanf("%d", &i);
+        ret = write(data_socket, &i, sizeof(int));
+        if (ret == -1) {
+            perror("write");
+            break;
+        }
+        printf("data sent = %d\n", i); 
+    } while(i);
+
+    memset(buffer, 0, BUFFER_SIZE);
+    strncpy (buffer, "RES", strlen("RES"));
+    buffer[strlen(buffer)] = '\0';
+    printf("buffer = %s\n", buffer);
+
+    ret = write(data_socket, buffer, strlen(buffer));
+    if (ret == -1) {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(buffer, 0, BUFFER_SIZE);
+    
+    ret = read(data_socket, buffer, BUFFER_SIZE);
+    if (ret == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[BUFFER_SIZE - 1] = 0;
+    printf("Result = %s\n", buffer);
+
+    close(data_socket);
+
+    exit(EXIT_SUCCESS);
+}
