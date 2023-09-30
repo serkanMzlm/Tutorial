ad9c52f8a876d78f8c60e17d546c5291d412a0c6
serkan <serkanmazlum306@gmail.com>
2023-09-30 17:34:57 +0300

    bir sınıftan bağımsız bir RCLCPP_INFO eklemesi yaptım

--- ros2/cpp_node/src/my_oop_node.cpp
+++ ros2/cpp_node/src/my_oop_node.cpp
@@ -3,6 +3,7 @@
 
 MyNode::MyNode(): Node("oop_node_cpp"), counter_(0){
     RCLCPP_INFO(this->get_logger(), "INFO");
+    RCLCPP_INFO(rclcpp::get_logger("new logger"), "new Logger...");
     RCLCPP_DEBUG(this->get_logger(), "DEBUG");
     RCLCPP_WARN(this->get_logger(), "WARN");
     RCLCPP_ERROR(this->get_logger(), "ERROR");
