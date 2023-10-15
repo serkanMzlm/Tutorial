9bdfa087e275de1114e31175c769c94afd36f30f
serkan <serkanmazlum306@gmail.com>
2023-10-15 16:40:34 +0300

    main fonksiyonu maskelendi

---
+++ c/hello_2.c
@@ -0,0 +1,10 @@
+/*
+* A C code that will print "Hello" without using the main() function.
+*/
+
+#include<stdio.h>    
+#define startup main   
+
+void startup() {    
+   printf("Hello\n");    
+}
