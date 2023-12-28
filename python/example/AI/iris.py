c69c1b63db1437a119d8f24efa5a3b3a6467c1c3
serkan <serkanmazlum306@gmail.com>
2023-12-28 15:33:42 +0300

    The prediction accuracy rate was calculated

--- python/example/AI/iris.py
+++ python/example/AI/iris.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier 
 
 iris = load_iris()
 # print(iris.keys())
@@ -12,7 +15,7 @@ iris = load_iris()
 # print(type(iris['data']))
 #########################################
 
-print("Toplam Data",iris['data'].shape) # Datanın boyutlarını gösterir.
+print("Data",iris['data'].shape) # Datanın boyutlarını gösterir.
 print("___________________________________________________________")
 
 # Veriler Test ve eğitim olarak iki gruba ayrılır
@@ -28,6 +31,21 @@ print("Y Test: ", y_test.shape)
 iris_df = pd.DataFrame(x_train, columns=iris.feature_names)
 
 # Saçılım matrix grafiği çizilir.
-pd.plotting.scatter_matrix(iris_df, c = y_train, figsize=(15, 15), marker='o',
-                        hist_kwds={'bins': 20}, s = 80, alpha = 0.8)
-plt.show()
+# pd.plotting.scatter_matrix(iris_df, c = y_train, figsize=(15, 15), marker='o',
+#                         hist_kwds={'bins': 20}, s = 80, alpha = 0.8)
+# plt.show()
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(x_train, y_train)
+
+x_new = np.array([[7.5, 5, 7, 0.2]])
+predict = knn.predict(x_new)
+
+print("Predict Calss:", predict)
+print("Predict Type:", iris['target_names'][predict])
+
+# Tahinin Doğruluk oranı
+y_predict = knn.predict(x_test)
+print("Accuracy rate:", np.mean(y_predict == y_test))
+# ya da 
+print("Accuracy rate:", knn.score(x_test, y_test))
