0220b6b500e5b4e603bb8b9e360178efca3c4114
serkan <serkanmazlum306@gmail.com>
2024-01-04 09:18:47 +0300

    Loading bar has been created

---
+++ cpp/loading_bar.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <thread>  // Eklenen kütüphane
+
+int main() {
+    const int totalSteps = 51;
+
+    for (int i = 0; i <= totalSteps; ++i) {
+        float percentage = static_cast<float>(i) / totalSteps * 100.0;
+        int barWidth = 50;
+
+        std::cout << "\r" << std::setw(3) <<"|";
+        int pos = barWidth * percentage / 100.0;
+        for (int j = 0; j < barWidth; ++j) {
+            if (j < pos) std::cout << "=";
+            else if (j == pos) std::cout << ">";
+            else std::cout << " ";
+        }
+        std::cout << "| " << i << "/" << totalSteps << " [" << std::fixed << std::setprecision(2) << percentage << "%]";
+        std::flush(std::cout);
+
+        // 0.1 saniye bekleme
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
