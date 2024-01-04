8bb60fd441b2bff73d3630b80599c3b1d63c2e11
serkan <serkanmazlum306@gmail.com>
2024-01-04 09:45:59 +0300

    A loading bar with new colors has been added

---
+++ cpp/loading_bar_color_2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <thread>
+
+// ANSI Escape Codes for text color
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define YELLOW  "\033[33m"
+#define GREEN   "\033[32m"
+
+int main() {
+    const int totalSteps = 51;
+
+    for (int i = 0; i <= totalSteps; ++i) {
+        float percentage = static_cast<float>(i) / totalSteps * 100.0;
+        int barWidth = 50;
+
+        std::cout << "\r" << "[" << i << "/" << totalSteps << "]" << std::setw(2) <<"|";
+        int pos = barWidth * percentage / 100.0;
+        for (int j = 0; j < barWidth; ++j) {
+            if (j < pos) std::cout << "=";
+            else if (j == pos) std::cout << ">";
+            else std::cout << " ";
+        }
+        std::cout << "|" << "[" << std::fixed;
+        if (percentage <= 35.0)
+            std::cout << RED << std::setprecision(2) << percentage << "%" << RESET << "]";
+        else if (percentage <= 75.0)
+            std::cout << YELLOW << std::setprecision(2) << percentage << "%" << RESET << "]";
+        else
+            std::cout << GREEN << std::setprecision(2) << percentage << "%" << RESET << "]";
+        std::flush(std::cout);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
