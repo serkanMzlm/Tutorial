02d28521eccd48c4d1f6da0fff5469157f602ea5
serkan <serkanmazlum306@gmail.com>
2024-04-18 16:15:09 +0300

    function point eklendi

--- c/function_pointer.c
+++ c/function_pointer.c
@@ -1,18 +1,32 @@
-#include <stdio.h>
+#include <stdio.h> 
+void add(int a, int b) 
+{ 
+    printf("Addition is %d\n", a+b); 
+} 
+void subtract(int a, int b) 
+{ 
+    printf("Subtraction is %d\n", a-b); 
+} 
+void multiply(int a, int b) 
+{ 
+    printf("Multiplication is %d\n", a*b); 
+} 
+  
+int main() { 
+    void (*fun_ptr_arr[])(int, int) = {add, subtract, multiply}; 
+    unsigned int ch, a = 15, b = 10; 
+  
+    printf("Enter Choice: 0 for add, 1 for subtract and 2 "
+            "for multiply\n"); 
+    scanf("%d", &ch); 
+  
+    if (ch > 2) return 0; 
+  
+    (*fun_ptr_arr[ch])(a, b); 
 
-// İki tamsayıyı toplayan fonksiyon
-int add(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    // Fonksiyon işaretçisi tanımlanıyor ve "add" fonksiyonuna işaret ediliyor
-    int (*addPtr)(int, int) = add;
-
-    // İşaretçi ile fonksiyon çağrısı yapılıyor
-    int result = addPtr(3, 5);
-
-    printf("Toplam: %d\n", result);
-
-    return 0;
-}
+    #if 0
+    #endif
+    #el
+  
+    return 0; 
+} 
