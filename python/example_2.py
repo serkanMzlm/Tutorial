24e75aaf4f18edf024c76057ad09ec4576c5fd37
serkan <serkanmazlum306@gmail.com>
2024-04-21 19:20:57 +0300

    listeler üzerinde çalışma yapıldı

--- python/example_2.py
+++ python/example_2.py
@@ -8,3 +8,12 @@ if var.isupper():
     print("isupper")
 elif var.islower():
     print("islower")
+
+text1 = "Hello123"
+text2 = "Hello 123"
+text3 = "Hello!"
+text4 = "123"
+print(text1.isalnum())  # Çıktı: True
+print(text2.isalnum())  # Çıktı: False (boşluk karakteri var)
+print(text3.isalnum())  # Çıktı: False (semboller var)
+print(text4.isalnum())  # Çıktı: False (semboller var)
