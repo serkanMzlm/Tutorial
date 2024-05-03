505584334929114ea4ee7fce51a785454ab2786d
serkan <serkanmazlum306@gmail.com>
2024-05-03 08:45:57 +0300

    pandas eklendi

--- python/modules/pandas_module.py
+++ python/modules/pandas_module.py
@@ -2,4 +2,17 @@
 import pandas as pd
 
 series = pd.Series(['BMW', 'Toyota', 'Honda'])
-print(series)
+color = pd.Series(["Red", "Blue", "Green"])
+
+car_data = pd.DataFrame({"Car Frame":series, "Color":color}) #İki farklı Seriyi birleştirilmesini sağlar.
+print(car_data, end="\n____________\n\n") 
+
+car_sales = pd.read_csv("car_data.csv")  #Belirli bir csv dosyayı içe aktarmayı sağlar.
+print(car_sales)
+
+
+# Belili bir değişiklik yapıldıktan sonra faklı çıktı olarak kaydedilebilir.
+# Aynı şekilde to_dict, to_excel... farklı veri dosyasına çevrile bilir. csv 
+# dosyasının geçerliliği her yerde olduğu için kullanılır. index = False sayesinde 
+# satır sayıları tekrar alınmaz 
+car_sales.to_csv("export_car_sales.csv", index = False) 
