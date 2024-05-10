fb3a79227a4c89a2d144ea37d70f0c5ff228d72e
serkan <serkanmazlum306@gmail.com>
2024-05-10 17:31:15 +0300

    cross duzenlendi

--- python/AI/cross_valiation.py
+++ python/AI/cross_valiation.py
@@ -1,8 +1,10 @@
 #!/bin/usr/python3
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+from sklearn.metrics import roc_curve
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -35,3 +37,26 @@ crossval_score = np.mean(cross_val_score(model, X, y, cv=5))
 debug(1, cross_val_score(model, X, y, cv=5)) 
 print("single score:", single_score, "\ncross_vall score:", crossval_score)
 
+y_probs = model.predict_proba(X_test)
+
+# [0][1] index de bulunan veriler alınır
+y_probs_positive = y_probs[:, 1]
+fpr, tpr, thresholds = roc_curve(y_test, y_probs_positive)
+def plot_roc_curve(fpr, tpr):
+    """
+    Plots a ROC curve given the false positive rate (fpr)
+    and true positive rate (tpr) of a model.
+    """
+    # Plot roc curve
+    plt.plot(fpr, tpr, color="orange", label="ROC")
+    plt.plot([0, 1], [0, 1], color="darkblue", linestyle="--", label="Guessing")
+    
+    plt.xlabel("False positive rate (fpr)")
+    plt.ylabel("True positive rate (tpr)")
+    plt.title("Receiver Operating Characteristic (ROC) Curve")
+    plt.legend()
+    plt.show()
+
+plot_roc_curve(fpr, tpr)
+
+plt.show()
