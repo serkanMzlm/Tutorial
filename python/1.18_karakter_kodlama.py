917e988a5134568b49de871c7cd8eb2d5ce01acf
serkan <serkanmazlum306@gmail.com>
2024-05-18 18:01:39 +0300

    fonksiyonlar kısaltıldı

--- python/1.18_karakter_kodlama.py
+++
@@ -1,5 +0,0 @@
-print (repr("\n")) #normalde gozukmeyen kacis satirlarini gosterilmesine yarar
-print (r"\n") #repr kisaltilmis hali
-print (ascii("\n")) #repr ayni
-print (chr(10)) # sayilarin karsiligi gosterir
-print (ord("\n")) #chr tam tersi  sayi karsiligini verir.
