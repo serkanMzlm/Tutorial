6b06923ee27ff9c05ca9950ba552114d0ac9362b
serkan <serkanmazlum306@gmail.com>
2024-05-18 18:11:55 +0300

    Gereksiz dosyalar silindi

--- python/example_1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-metin = "Bu yAzıdA küçük A yok."
-for harf in metin:
-        if harf == 'a':
-                print("a harfi bulundu.")
-                break
-else:
-        print("a harfi bulunmadı.")
--- python/recursion.py
+++
@@ -1,8 +0,0 @@
-def azalt(s):
-    if len(s) < 1:
-        return s
-    else:
-        print(s)
-        return azalt(s[1:])
-
-azalt('istihza')
