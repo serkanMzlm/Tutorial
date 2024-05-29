877d2b11b9a9e9328408e61bb3802d6347f46eed
serkan <serkanmazlum306@gmail.com>
2024-05-29 15:22:14 +0300

    sınıflandırma düzenlendi

--- cpp/belirli_kurala_gore_gruplama.cpp
+++ cpp/belirli_kurala_gore_gruplama.cpp
@@ -1,81 +1,84 @@
-/*
- * İki boyutlu dizin içinde bulunan 1'lerin gruplanması
- *       a) Sağ, sol, alt, üst kısımdakileri gruplama
- *       b) Sağ, sağ alt, sağ üst... temas eden herhangi
- *          bir yüzey
- * Çözüm: DFS (Depth-First Search)
-*/
+// İki boyutlu dizin içinde bulunan 1'lerin gruplanması
+//       a) Sağ, sol, alt, üst kısımdakileri gruplama
+//       b) Sağ, sağ alt, sağ üst... temas eden herhangi
+//          bir yüzey
+// Çözüm: DFS (Depth-First Search)
 
 #include <iostream>
 #include <vector>
 
-using namespace std;
-int rows, cols;
+std::vector<std::vector<int>> matrix = {
+    {1, 0, 1},
+    {1, 0, 0},
+    {0, 0, 1}
+};
 
-// A
-void dfs(std::vector<std::vector<int>> &matrix, std::vector<std::vector<bool>> &visited,
-                                int row, int col, std::vector<std::pair<int, int>> &indices)
+int cols = matrix.size(); 
+int rows = matrix[0].size();
+
+typedef struct
 {
+    std::vector<std::vector<int>> matrix;
+    std::vector<std::vector<bool>> visited;
+    std::vector<std::pair<int, int>> indices;
+    std::vector<std::vector<std::pair<int, int>>> groups;
+} DFS_s;
 
-    // sağ, sol, alt, üst
-    static int directions = 4;
-    // Hareket Yönleri (sol, sağ, üst, alt)
-    static int dRow[] = {0, 0, -1, 1};
-    static int dCol[] = {-1, 1, 0, 0};
+int dRow[4] = {0, 0, -1, 1};
+int dCol[4] = {-1, 1, 0, 0};
 
-    visited[row][col] = true; // Hücre ziyaret edilmiş olarak işaretlenir
-    indices.push_back({row, col});
+void dfs(DFS_s &data, int row, int col);
 
-    for (int i = 0; i < directions; i++){
-        int newRow = row + dRow[i];
-        int newCol = col + dCol[i];
-
-        if (newRow >= 0 && newRow < rows && newCol >= 0 && 
-                newCol < cols && matrix[newRow][newCol] == 1 
-                                    && !visited[newRow][newCol]) {
-                dfs(matrix, visited, newRow, newCol, indices);
-            }
-    }
-}
-
-int main()
+int main(int argc, char** argv)
 {
-    std::vector<std::vector<int>> matrix = {
-        {1, 0, 0},
-        {1, 0, 0},
-        {0, 0, 1}
-    };
+    DFS_s data;
+    std::vector<std::vector<bool>> visited(rows, std::vector<bool>(cols, false));
 
-    rows = matrix.size();
-    cols = matrix[0].size();
+    data.matrix = matrix;
+    data.visited = visited;
 
-    // Ziyaret edilen hücreleri tutmak için vektör
-    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-
-    // Grupları ve indekslerini tutmak için vektör
-    vector<vector<pair<int, int>>> groups;
-
-    // Matris üzerindeki her hücreyi gez
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             // Eğer hücre değeri 1 ise ve ziyaret edilmemişse, yeni bir grup başlat
-            if (matrix[i][j] == 1 && !visited[i][j]) {
-                vector<pair<int, int>> indices;
-                dfs(matrix, visited, i, j, indices);
-                groups.push_back(indices);
+            if (data.matrix[i][j] == 1 && !data.visited[i][j]) {
+                dfs(data, i, j);
+                data.groups.push_back(data.indices);
+                data.indices.clear();
             }
         }
     }
 
-    // Grupları ve indekslerini yazdır
-    cout << "Toplam grup sayısı: " << groups.size() << endl;
-    for (int i = 0; i < groups.size(); i++) {
-        cout << "Grup " << i + 1 << ": ";
-        for (const auto& idx : groups[i]) {
-            cout << "(" << idx.first << ", " << idx.second << ") ";
+    std::cout << "Toplam grup sayısı: " << data.groups.size() << std::endl;
+    for (int i = 0; i < data.groups.size(); i++) {
+        std::cout << "Grup " << i + 1 << ": ";
+        for (const auto& idx : data.groups[i]) {
+            std::cout << "(" << idx.first << ", " << idx.second << ") ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 
     return 0;
+}
+
+void dfs(DFS_s &data, int row, int col)
+{
+    data.visited[row][col] = true;
+    data.indices.push_back({row, col});
+
+    for(int i = 0; i < 4; i++){
+        int newRow = row + dRow[i]; 
+        int newCol = col + dCol[i]; 
+
+        bool is_limit = (newCol >= 0 && newCol < cols && newRow >= 0 && newRow < rows);
+        if(!is_limit)
+        {
+            continue;
+        } 
+
+        bool is_group = data.matrix[newRow][newCol] && !data.visited[newRow][newCol];
+        if(is_group )
+        {
+            dfs(data, newRow, newCol);
+        }
+    }
 }
