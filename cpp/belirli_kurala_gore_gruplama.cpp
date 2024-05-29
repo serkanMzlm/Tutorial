c5e8f871c86850f9594973352653d4ad98d11204
serkan <serkanmazlum306@gmail.com>
2024-05-29 15:30:37 +0300

    ekrana basmak için fonksiyon eklendi

--- cpp/belirli_kurala_gore_gruplama.cpp
+++ cpp/belirli_kurala_gore_gruplama.cpp
@@ -10,10 +10,9 @@
 std::vector<std::vector<int>> matrix = {
     {1, 0, 1},
     {1, 0, 0},
-    {0, 0, 1}
-};
+    {0, 0, 1}};
 
-int cols = matrix.size(); 
+int cols = matrix.size();
 int rows = matrix[0].size();
 
 typedef struct
@@ -28,19 +27,26 @@ int dRow[4] = {0, 0, -1, 1};
 int dCol[4] = {-1, 1, 0, 0};
 
 void dfs(DFS_s &data, int row, int col);
+void printData(DFS_s data);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    DFS_s data;
     std::vector<std::vector<bool>> visited(rows, std::vector<bool>(cols, false));
 
-    data.matrix = matrix;
-    data.visited = visited;
+    DFS_s data = {
+        matrix,
+        visited,
+        {},
+        {}
+    };
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
             // Eğer hücre değeri 1 ise ve ziyaret edilmemişse, yeni bir grup başlat
-            if (data.matrix[i][j] == 1 && !data.visited[i][j]) {
+            if (data.matrix[i][j] == 1 && !data.visited[i][j])
+            {
                 dfs(data, i, j);
                 data.groups.push_back(data.indices);
                 data.indices.clear();
@@ -48,15 +54,7 @@ int main(int argc, char** argv)
         }
     }
 
-    std::cout << "Toplam grup sayısı: " << data.groups.size() << std::endl;
-    for (int i = 0; i < data.groups.size(); i++) {
-        std::cout << "Grup " << i + 1 << ": ";
-        for (const auto& idx : data.groups[i]) {
-            std::cout << "(" << idx.first << ", " << idx.second << ") ";
-        }
-        std::cout << std::endl;
-    }
-
+    printData(data);
     return 0;
 }
 
@@ -65,20 +63,35 @@ void dfs(DFS_s &data, int row, int col)
     data.visited[row][col] = true;
     data.indices.push_back({row, col});
 
-    for(int i = 0; i < 4; i++){
-        int newRow = row + dRow[i]; 
-        int newCol = col + dCol[i]; 
+    for (int i = 0; i < 4; i++)
+    {
+        int newRow = row + dRow[i];
+        int newCol = col + dCol[i];
 
         bool is_limit = (newCol >= 0 && newCol < cols && newRow >= 0 && newRow < rows);
-        if(!is_limit)
+        if (!is_limit)
         {
             continue;
-        } 
+        }
 
         bool is_group = data.matrix[newRow][newCol] && !data.visited[newRow][newCol];
-        if(is_group )
+        if (is_group)
         {
             dfs(data, newRow, newCol);
         }
+    }
+}
+
+void printData(DFS_s data)
+{
+    std::cout << "Toplam grup sayısı: " << data.groups.size() << std::endl;
+    for (int i = 0; i < data.groups.size(); i++)
+    {
+        std::cout << "Grup " << i + 1 << ": ";
+        for (const auto &idx : data.groups[i])
+        {
+            std::cout << "(" << idx.first << ", " << idx.second << ") ";
+        }
+        std::cout << std::endl;
     }
 }
