1a230d49c3b99903a2c57ed3595b27f356350b7b
serkan <serkanmazlum306@gmail.com>
2024-07-19 14:47:59 +0300

    gazebo pub ve sub düzeltildi

--- gazebo/src/topic/subscriber.cc
+++ gazebo/src/topic/subscriber.cc
@@ -6,12 +6,17 @@ void cb(const gz::msgs::StringMsg &_msg)
 {
   std::cout << "Msg: " << _msg.data() << std::endl << std::endl;
 }
+void cb2(const gz::msgs::Int32 &_msg)
+{
+  std::cout << "Msg 2: " << _msg.data() << std::endl << std::endl;
+}
 int main(int argc, char **argv)
 {
   gz::transport::Node node;
   std::string topic = "/foo";
+  std::string topic_2 = "/motor_pub_flags";
   // Subscribe to a topic by registering a callback.
-  if (!node.Subscribe(topic, cb))
+  if (!node.Subscribe(topic, cb) || !node.Subscribe(topic_2, cb2))
   {
     std::cerr << "Error subscribing to topic [" << topic << "]" << std::endl;
     return -1;
