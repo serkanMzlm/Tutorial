d76b57e3bedb229626833df736add5b47f9f5801
serkan <serkanmazlum306@gmail.com>
2024-09-09 14:12:40 +0300

    grafik oluştuma düzenlendi

--- matlab/a2_temel_grafik.m
+++ matlab/a2_temel_grafik.m
@@ -1,11 +1,35 @@
-clc;
-clear;
-clear all;
-x = 0:1:10;
-y = x;
-plot(x,y) %x ve y eksenleri verilen grafik
-grid off %grid ekran seklinde gösterir parametre  olarak "on" ve "off" kullanılır
-xlabel("x ekseni") %x eksenine isim veriririz
-ylabel("y ekseni") %y eksenine isim veririz
-title("Başlık") %grafiğe başlık vermek 
-axis([0 5 0 5]) %grafikte gösterilecek aralığı gösteriri ilk iki hane x sonra ki ye
+clc;      % Konsol Ekranını Temizler
+clear;    % Workspace Kısmında Bulunan Verilerin Silinmesini Sağlar
+ 
+x_label =    "Time [s]";
+y_label =    "Position [m]";
+y_label_2 =    "Position 2 [m]";
+main_title = "Position / Time Graph";
+
+t = 0:0.1:2*pi;
+a = sin(t);
+b = 125*cos(t);
+
+% 'hold on' Grafiklerde bulunan değerlerin saklanmasını sağlar. Bu sayede
+% bir grafiğin üstünde farklı veriler gösterilebiliriz. plot komutu x, y
+% eksenine kullanılıcak veriler ve oluşucak çizginin rengi ve kalınlığının
+% ayarlanmasını sağlar.
+hold on
+yyaxis left; % Değerlerin hangi tarafta olucağını ayarlar.
+plot(t, a, 'r', LineWidth=2);
+xlabel(x_label, FontSize=10); % x ekseni başlığı ve font boyutu
+ylabel(y_label, FontSize=10); % y ekseni başlığı ve font boyutu
+title(main_title, FontSize=10); % Ana başlık ve font boyutu
+text(1, 0, "Theta is 90") % grafiğin belirli noktasında yazı yazmayı sağlar
+axis([0 6 -1 1])          % grafiğin gösteriliceği aralık
+
+yyaxis right;
+plot(t, b, 'b', LineWidth=2);
+ylabel(y_label_2, FontSize=10); % y ekseni başlığı ve font boyutu
+axis([0 6 -150 150])          % grafiğin gösteriliceği aralık
+
+grid on; % grid aktif veya pasif olmasını sağlar.
+
+% Grafikte  cizgilerin isimlendirmemizi saglar lambda , pi ,epsilon gibi 
+ % simgeleri kullanmak için \ kullanılır.
+legend("sin", "cos")     
