044f6725681199327cd91e7922a34931078b33dd
serkan <serkanmazlum306@gmail.com>
2024-10-09 17:34:57 +0300

    ekran start stop eklendi

--- qt/conveyor_2/serial_comm.cpp
+++ qt/conveyor_2/serial_comm.cpp
@@ -5,6 +5,8 @@ SerialComm::SerialComm(QObject *parent)
 {
     port_ = new QSerialPort(this);
     checkDeviceConnection();
+    debugPort();
+    connect(port_, &QSerialPort::readyRead, this, &SerialComm::readBytes);
 }
 
 SerialComm::~SerialComm()
@@ -14,16 +16,18 @@ SerialComm::~SerialComm()
 
 void SerialComm::checkDeviceConnection()
 {
-    for(const QSerialPortInfo port_list : QSerialPortInfo::availablePorts()){
+    for(const QSerialPortInfo& port_list : QSerialPortInfo::availablePorts()){
         bool is_found = ((port_list.manufacturer() == "FTDI") ||
                          (port_list.description() == "STLINK-V3") ||
-                         (port_list.description() == "USB-SERIAL CH340"));
+                         (port_list.description() == "USB-SERIAL CH340") ||
+                         (port_list.manufacturer() == "STMicroelectronics"));
         if(is_found){
             port_->setPortName(port_list.portName());
             port_->setBaudRate(BAUDRATE);
             port_->setDataBits(QSerialPort::Data8);
             while(port_->open(QIODevice::ReadWrite)){ }
             qDebug() << "Port created successfully";
+            setInfo("Port created successfully");
             QThread::sleep(1);
         }
     }
@@ -31,6 +35,15 @@ void SerialComm::checkDeviceConnection()
 
 void SerialComm::readBytes()
 {
+    while(port_->bytesAvailable()){
+        uint8_t cur_byte;
+        port_->read((char*)&cur_byte, 1);
+        if(cur_byte == 46)
+        {
+            setInfo("Enter a Number");
+            start_test = true;
+        }
+    }
 }
 
 void SerialComm::writeBytes(uint8_t new_data)
@@ -38,9 +51,17 @@ void SerialComm::writeBytes(uint8_t new_data)
     if ( !(port_->isOpen()))
     {
         qDebug() << "Port Closed";
+        setError("Port Closed");
         return ;
     }
 
+    if(!start_test && new_data !=  0x11)
+    {
+        setError("Device Not Ready");
+        return;
+    }
+    setError("");
+    QString new_data_str = QString::fromStdString(std::to_string(static_cast<int>(new_data)));
     char dataToSend[1] = { static_cast<char>(new_data) };
     qint64 bytesWritten = port_->write(dataToSend, 1);
     if (bytesWritten == -1) {
@@ -51,7 +72,7 @@ void SerialComm::writeBytes(uint8_t new_data)
 }
 
 void SerialComm::debugPort(){
-    for(const QSerialPortInfo port_list : QSerialPortInfo::availablePorts()){
+    for(const auto &port_list : QSerialPortInfo::availablePorts()){
         if(port_list.description() == "") {
             continue;
         }
@@ -64,4 +85,26 @@ void SerialComm::debugPort(){
         qDebug() << "vendorIdentifier:  " << port_list.vendorIdentifier();
         qDebug() << "productIdentifier: " << port_list.productIdentifier();
     }
+}
+
+QString SerialComm::error() const
+{
+    return m_error;
+}
+
+void SerialComm::setError(const QString &newError)
+{
+    m_error = newError;
+    emit errorChanged();
+}
+
+QString SerialComm::info() const
+{
+    return m_info;
+}
+
+void SerialComm::setInfo(const QString &newInfo)
+{
+    m_info = newInfo;
+    emit infoChanged();
 }
--- qt/conveyor_2/serial_comm.h
+++ qt/conveyor_2/serial_comm.h
@@ -12,8 +12,14 @@
 class SerialComm : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString error READ error WRITE setError NOTIFY errorChanged FINAL)
+    Q_PROPERTY(QString info READ info WRITE setInfo NOTIFY infoChanged FINAL)
 private:
     QSerialPort *port_;
+    QString m_error = "";
+    QString m_info = "";
+    bool start_test = false;
+
 public:
     explicit SerialComm(QObject *parent = nullptr);
     ~SerialComm();
@@ -21,7 +27,15 @@ public:
     void readBytes();
     Q_INVOKABLE void writeBytes(uint8_t new_data = 0x11);
     Q_INVOKABLE void debugPort();
+    QString error() const;
+    void setError(const QString &newError);
+
+    QString info() const;
+    void setInfo(const QString &newInfo);
+
 signals:
+    void errorChanged();
+    void infoChanged();
 };
 
 #endif // SERIAL_COMM_H
